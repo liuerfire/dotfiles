ce7dead3faf6bca09d6ff6403cd9a2d905bd642b
liuerfire <liuerfire@gmail.com>
2024-04-03 22:33:36 +0800

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,10 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
 vim.opt.list = true
 vim.opt.listchars = "tab:⇢ ,eol:¬,trail:·,extends:↷,precedes:↶"
 vim.opt.showbreak = "↪"
@@ -289,6 +293,9 @@ require("lazy").setup({
         },
         sync_install = false,
         highlight = {
+          enable = true,
+        },
+        indent = {
           enable = true,
         },
         incremental_selection = {
