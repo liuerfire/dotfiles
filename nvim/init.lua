b00761409a4ce3f3b6ec3dcdf9c16c9bc7169319
liuerfire <liuerfire@gmail.com>
2024-09-12 20:50:19 +0800

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -657,10 +657,6 @@ local jdtls_config = {
       configuration = {
         runtimes = {
           {
-            name = "JavaSE-11",
-            path = "/usr/lib/jvm/java-11-openjdk/",
-          },
-          {
             name = "JavaSE-17",
             path = "/usr/lib/jvm/java-17-openjdk/",
           },
